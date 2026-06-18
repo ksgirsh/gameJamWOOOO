3a81d5dda413bc8a359d99a82a076b45ffa430e5
mld <krishgovind2019@gmail.com>
2026-06-18 18:45:28 -0500

    Finished Basic Movement

--- Assets/Scripts/GridMovement.cs
+++ Assets/Scripts/GridMovement.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class GridMovement : MonoBehaviour
 {
-    public int cellSize;
+    public Grid mainGrid;
+    private float cellSize;
     private Vector2 initPosition;
 
     private float movementX;
     private float movementY;
+    private bool moving;
 
     Vector2 gridPosition;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int initSpot = (cellSize / 2);
-        initPosition = new Vector2(0.5f, 0.5f);
+        cellSize = mainGrid.cellSize.x;
+        float initSpot = (mainGrid.cellSize.x / 2);
+        initPosition = new Vector2(initSpot, initSpot);
         Debug.Log(initSpot);
         transform.position = initPosition;
         gridPosition = initPosition;
@@ -25,22 +29,39 @@ public class GridMovement : MonoBehaviour
         movementX = Input.GetAxisRaw("Horizontal");
         movementY = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetButtonDown("Horizontal"))
+        if (Input.GetButtonDown("Horizontal") && !moving)
         {
-            gridPosition.x += movementX;
-            transform.position = gridPosition;
+            gridPosition.x += (movementX * cellSize);
+            StartCoroutine(ShiftToCell(gridPosition));
         }
 
-        if (Input.GetButtonDown("Vertical"))
+        if (Input.GetButtonDown("Vertical") && !moving)
         {
-            gridPosition.y += movementY;
-            transform.position = gridPosition;
+            gridPosition.y += (movementY * cellSize);
+            StartCoroutine(ShiftToCell(gridPosition));
         }
     }
 
     void FixedUpdate()
     {
         
+
+    }
+
+    IEnumerator ShiftToCell(Vector2 worldCellPos, float lerpPoint = 0.75f, float duration = 0.02f)
+    {
+        //worldCellPos just equals futureGridPosition
+        Vector2 initPosition = transform.position;
+
+        Vector2 startLerpPoint = Vector2.Lerp(initPosition, worldCellPos, lerpPoint);
+        moving = true;
+        for (float i = 0; i < duration; i += Time.deltaTime)
+        {
+            transform.position = Vector2.Lerp(startLerpPoint, worldCellPos, i);
+            yield return null;
+        }
+        transform.position = worldCellPos;
+        moving = false;
 
     }
 }
